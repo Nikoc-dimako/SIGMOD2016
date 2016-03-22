02c9322d0a31a09ca8009de1a98653b0da9fce08
Thanasulas <tkarabatsis@hotmail.com>
2016-03-23 00:48:00 +0200

    Vectors array with addition/deletion flag + more
    Also preparing for the rest of the functions for multithread

--- main.cpp
+++ main.cpp
@@ -20,8 +20,14 @@ using namespace std;
 #define MAXN ((size_t)1<<24)
 typedef unsigned int Node;
 
+typedef struct GraphNode {
+   bool addition;
+   bool deletion;
+   vector<Node> nodes;
+} GraphNode;
+
 // Variables for the Queues
-vector<Node> *ForwardGraph, *BackwardGraph;
+GraphNode *ForwardGraph, *BackwardGraph;
 Node *visited, *dist;
 Node *fQueue;
 Node *bQueue;
@@ -38,9 +44,9 @@ int shortest_path(Node a, Node b) {
 	visitedCounter += 2;
 
 	// Initialize Queues and Variables
-	unsigned int fChildrenCount = ForwardGraph[a].size();
+	unsigned int fChildrenCount = ForwardGraph[a].nodes.size();
 	unsigned int fCurrentNodes = 1;
-	unsigned int bChildrenCount = BackwardGraph[b].size();
+	unsigned int bChildrenCount = BackwardGraph[b].nodes.size();
 	unsigned int bCurrentNodes = 1;
 
 	unsigned int fQueuePointer = 0;
@@ -57,8 +63,8 @@ int shortest_path(Node a, Node b) {
 	dist[b] = 0;
 
 	unsigned int iterator;
-	unsigned int currentFather;
-	unsigned int currentChild;
+	Node currentFather;
+	Node currentChild;
 
 	// The queue saves the currently explored nodes
 	// The children counters contain the number of children that the already explored nodes have.
@@ -67,26 +73,70 @@ int shortest_path(Node a, Node b) {
 	while(1){
 		iterator = 0;
 
-		if(fChildrenCount < bChildrenCount){	// Move forward, there are less children there
+		if(fChildrenCount <= bChildrenCount){	// Move forward, there are less children there
 			fChildrenCount = 0;
 			fGraphDistance++;	// Going to the next distance
+
+			// Reading all the children from the nodes in the queue
 			for(unsigned int i=0; i < fCurrentNodes; i++){
 				currentFather = fQueue[i+fQueuePointer*MAXN];
-				for(unsigned int j=0; j < ForwardGraph[currentFather].size(); j++){
-					currentChild = ForwardGraph[currentFather][j];
 
-					if(visited[currentChild] >= visitedCounter){	// Explored by the other side
-						if(visited[currentChild] == visitedCounter+1) 	// Found the minimum distance!
-							return dist[currentChild] + dist[currentFather] + 1;
-						continue;
+				// Check if this currentFather node has any deletions done at him
+				if(ForwardGraph[currentFather].deletion == 0){	// Just check the children nodes
+
+					// Reading the children of the current node in the queue
+					for(unsigned int j=0; j < ForwardGraph[currentFather].nodes.size(); j++){
+						currentChild = ForwardGraph[currentFather].nodes[j];
+						if(visited[currentChild] >= visitedCounter){	// Explored by the other side
+							if(visited[currentChild] == visitedCounter+1) 	// Found the minimum distance!
+								return dist[currentChild] + dist[currentFather] + 1;
+							continue;
+						}
+						visited[currentChild] = visitedCounter;
+						dist[currentChild] = fGraphDistance;
+						fChildrenCount += ForwardGraph[currentChild].nodes.size();	// Counting the children of the next stage
+						fQueue[iterator+(fQueuePointer^1)*MAXN] = currentChild;
+						iterator++;
 					}
-					visited[currentChild] = visitedCounter;
-					dist[currentChild] = fGraphDistance;
-					fChildrenCount += ForwardGraph[currentChild].size();	// Counting the children of the next stage
-					fQueue[iterator+(fQueuePointer^1)*MAXN] = currentChild;
-					iterator++;
+				}else{
+					// We have to check at every child if it was deleted
+					for(unsigned int j=0; j < ForwardGraph[currentFather].nodes.size(); j++){
+						currentChild = ForwardGraph[currentFather].nodes[j];
+
+						// Check if the node was deleted
+						// If it was continue to the next child;
+						// TODO
+
+						if(visited[currentChild] >= visitedCounter){	// Explored by the other side
+							if(visited[currentChild] == visitedCounter+1) 	// Found the minimum distance!
+								return dist[currentChild] + dist[currentFather] + 1;
+							continue;
+						}
+						visited[currentChild] = visitedCounter;
+						dist[currentChild] = fGraphDistance;
+						fChildrenCount += ForwardGraph[currentChild].nodes.size();	// Counting the children of the next stage
+						fQueue[iterator+(fQueuePointer^1)*MAXN] = currentChild;
+						iterator++;
+					}
+				}
+
+				// Check if there were any additions in the current father node
+				if(ForwardGraph[currentFather].addition == 1){
+					// For every node added
+					if(ForwardGraph[currentFather].deletion == 0){
+
+						// Just add the nodes to the queue
+						// TODO
+
+					}else{
+
+						// You have to check if it was deleted first
+						// TODO
+
+					}
 				}
 			}
+
 			if(fChildrenCount == 0)
 				return -1;
 			fCurrentNodes = iterator;
@@ -95,23 +145,67 @@ int shortest_path(Node a, Node b) {
 		}else{
 			bChildrenCount = 0;
 			bGraphDistance++;	// Going to the next distance
+
+			// Reading all the children from the nodes in the queue
 			for(unsigned int i=0; i < bCurrentNodes; i++){
 				currentFather = bQueue[i+bQueuePointer*MAXN];
-				for(unsigned int j=0; j < BackwardGraph[currentFather].size(); j++){
-					currentChild = BackwardGraph[currentFather][j];
 
-					if(visited[currentChild] >= visitedCounter){	// Explored by the other side
-						if(visited[currentChild] == visitedCounter) 	// Found the minimum distance!
-							return dist[currentChild] + dist[currentFather] + 1;
-						continue;
+				// Check if this currentFather node has any deletions done at him
+				if(BackwardGraph[currentFather].deletion == 0){	// Just check the children nodes
+
+					// Reading the children of the current node in the queue
+					for(unsigned int j=0; j < BackwardGraph[currentFather].nodes.size(); j++){
+						currentChild = BackwardGraph[currentFather].nodes[j];
+						if(visited[currentChild] >= visitedCounter){	// Explored by the other side
+							if(visited[currentChild] == visitedCounter) 	// Found the minimum distance!
+								return dist[currentChild] + dist[currentFather] + 1;
+							continue;
+						}
+						visited[currentChild] = visitedCounter+1;
+						dist[currentChild] = bGraphDistance;
+						bChildrenCount += BackwardGraph[currentChild].nodes.size();	// Counting the children of the next stage
+						bQueue[iterator+(bQueuePointer^1)*MAXN] = currentChild;
+						iterator++;
 					}
-					visited[currentChild] = visitedCounter+1;
-					dist[currentChild] = bGraphDistance;
-					bChildrenCount += BackwardGraph[currentChild].size();	// Counting the children of the next stage
-					bQueue[iterator+(bQueuePointer^1)*MAXN] = currentChild;
-					iterator++;
+				}else{
+					// We have to check at every child if it was deleted
+					for(unsigned int j=0; j < BackwardGraph[currentFather].nodes.size(); j++){
+						currentChild = BackwardGraph[currentFather].nodes[j];
+
+						// Check if the node was deleted
+						// If it was continue to the next child;
+						// TODO
+
+						if(visited[currentChild] >= visitedCounter){	// Explored by the other side
+							if(visited[currentChild] == visitedCounter) 	// Found the minimum distance!
+								return dist[currentChild] + dist[currentFather] + 1;
+							continue;
+						}
+						visited[currentChild] = visitedCounter+1;
+						dist[currentChild] = bGraphDistance;
+						bChildrenCount += BackwardGraph[currentChild].nodes.size();	// Counting the children of the next stage
+						bQueue[iterator+(bQueuePointer^1)*MAXN] = currentChild;
+						iterator++;
+					}
+				}
+
+				// Check if there were any additions in the current father node
+				if(BackwardGraph[currentFather].addition == 1){
+					// For every node added
+					if(BackwardGraph[currentFather].deletion == 0){
+
+						// Just add the nodes to the queue
+						// TODO
+
+					}else{
+
+						// You have to check if it was deleted first
+						// TODO
+
+					}
 				}
 			}
+
 			if(bChildrenCount == 0)
 				return -1;
 			bCurrentNodes = iterator;
@@ -120,36 +214,50 @@ int shortest_path(Node a, Node b) {
 	}
 }
 
-void add_edge(Node a, Node b) {
-  ForwardGraph[a].push_back(b);
-  BackwardGraph[b].push_back(a);
+void add_edge_final(Node a, Node b) {
+  ForwardGraph[a].nodes.push_back(b);
+  BackwardGraph[b].nodes.push_back(a);
 }
 
 int _delete_edge(Node a, vector<Node> &E) {
-  int size = E.size(), newsize = 0;
-  for(int i=0; i < size; i++){
-    E[newsize] = E[i];
-    if(E[i] != a)
+	int size = E.size(), newsize = 0;
+	for(int i=0; i < size; i++){
+	E[newsize] = E[i];
+	if(E[i] != a)
 		newsize++;
-  }
-  if(newsize == size)
-	  return 0;
-  E.resize(newsize);
-  return 1;
+	}
+	if(newsize == size)
+		return 0;
+	E.resize(newsize);
+	return 1;
+}
+
+void delete_edge_final(Node a,Node b) {
+	if(!_delete_edge(b,ForwardGraph[a].nodes) || !_delete_edge(a,BackwardGraph[b].nodes) )
+		return;
+}
+
+void add_edge(Node a, Node b) {
+	// TODO
+	ForwardGraph[a].nodes.push_back(b);
+	BackwardGraph[b].nodes.push_back(a);
 }
 
 void delete_edge(Node a,Node b) {
-  if(!_delete_edge(b,ForwardGraph[a]) || !_delete_edge(a,BackwardGraph[b]) )
-	  return;
+	// TODO
+	if(!_delete_edge(b,ForwardGraph[a].nodes) || !_delete_edge(a,BackwardGraph[b].nodes) )
+		return;
 }
 
-bool sortF(Node a,Node b) { return (ForwardGraph[a].size()>ForwardGraph[b].size()); }
-bool sortB(Node a,Node b) { return (BackwardGraph[a].size()>BackwardGraph[b].size()); }
+
+// Sorting
+bool sortF(Node a,Node b) { return (ForwardGraph[a].nodes.size()>ForwardGraph[b].nodes.size()); }
+bool sortB(Node a,Node b) { return (BackwardGraph[a].nodes.size()>BackwardGraph[b].nodes.size()); }
 
 void preprocess() {
   for(unsigned int a=1; a < nameChangeCounter; a++) {
-    sort(ForwardGraph[a].begin(),ForwardGraph[a].end(),sortF);
-    sort(BackwardGraph[a].begin(),BackwardGraph[a].end(),sortB);
+    sort(ForwardGraph[a].nodes.begin(),ForwardGraph[a].nodes.end(),sortF);
+    sort(BackwardGraph[a].nodes.begin(),BackwardGraph[a].nodes.end(),sortB);
   }
 }
 
@@ -160,8 +268,8 @@ int main() {
 	Node a,b;
 	char c;
 
-	ForwardGraph = (vector<Node>*)calloc(MAXN, sizeof(vector<Node>));
-	BackwardGraph = (vector<Node>*)calloc(MAXN, sizeof(vector<Node>));
+	ForwardGraph = (GraphNode*)calloc(MAXN, sizeof(GraphNode));
+	BackwardGraph = (GraphNode*)calloc(MAXN, sizeof(GraphNode));
 	fQueue = (Node*)calloc(MAXN*2, sizeof(Node));
 	bQueue = (Node*)calloc(MAXN*2, sizeof(Node));
 	visited = (Node*)calloc(MAXN, sizeof(Node));
@@ -185,6 +293,10 @@ int main() {
 		if(c == 'F') {
 			cout << flush;
 			cin.clear();
+
+			// Add the additions/deletions of this batch to the main graph
+			// TODO
+
 			continue;
 		}
 
